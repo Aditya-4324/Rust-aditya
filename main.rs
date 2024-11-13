34c367f9bb491350bcc153f74ef479146e486633
Aditya-4324 <kumar.aditya5214@gmail.com>
2024-11-13 16:37:12 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,9 +3,12 @@ fn main(){
     let x: i32  = -5;
     let y: u32 = 1000;
     let z: f32 = 1000.001;
+    let b:i8 =2;
 
     print!("x: {}", x);
     print!("y: {}", y);   
     print!("z: {}", z);
+    print!("b: {}", b);
+    
 
 }
